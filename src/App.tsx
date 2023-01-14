9054690e6695e96b5129d8ac02863e665dd349ed
mami-inuzuka <inuzuka.mami@gmail.com>
2023-01-14 10:01:33 +0900

    ReactHookFormを用いてバリデーション付きのフォームを作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import { useForm } from 'react-hook-form';
 
 function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      firstName: 'Taro',
+      lastName: 'Yamada',
+    },
+  });
+  console.log(errors);
   return (
     <>
-      <form>
-        <input name="FirstName" placeholder="FirstName" />
-        <input name="LastName" placeholder="LastName" />
+      <form
+        onSubmit={handleSubmit((data) => {
+          console.log(data);
+        })}
+      >
+        <input {...register('firstName', { required: 'This is required.' })} placeholder="FirstName" />
+        <p>{errors.firstName?.message}</p>
+        <input
+          {...register('lastName', {
+            required: 'This is required.',
+            minLength: {
+              value: 4,
+              message: 'Min Length is 4.',
+            },
+          })}
+          placeholder="LastName"
+        />
+        <p>{errors.lastName?.message}</p>
         <input type="submit" />
       </form>
     </>
